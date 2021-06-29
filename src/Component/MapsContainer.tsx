a377b2771364a1b7d727786f5d626e71d2e7a80b
Jin <jeeyeon0523@gmail.com>
2021-06-29 23:18:57 +0900

    map drawer

--- src/Component/MapsContainer.tsx
+++ src/Component/MapsContainer.tsx
@@ -7,29 +7,105 @@ import './DetailContainer';
 import Maps from '../modules/Maps';
 import DetailContainer from './DetailContainer';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import { Drawer, ClickAwayListener } from '@material-ui/core';
 import styled from 'styled-components';
 import ContainerDimensions from "react-container-dimensions"
 import Navi from './Navi';
 import DetailPopup from './DetailPopup';
+import {useMapEvents} from 'react-leaflet';
+import { NONAME } from 'dns';
 
 const SFullScreen = styled.div`
   height : 100%;
   width : 100%;
 `;
 
+const drawerWidth = 350;
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginRight: drawerWidth,
+  },
+  title: {
+    flexGrow: 1,
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+    background : 'transparent'
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-start',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginRight: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginRight: 0,
+  },
+}));
 
 function MapsContainer() {
 
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(true);
+    const handleDrawerOpen = () => {
+        setOpen(true);
+    };
+    
+    const handleDrawerClose = () => {
+        setOpen(false);
+    };
+
     return (
-        
-        <SFullScreen>
+        <div>
             <Maps />
-
-            <DetailPopup/>
-
-        </SFullScreen>
+            <ClickAwayListener onClickAway={handleDrawerClose}>
+                <Drawer className={classes.drawer} variant="persistent" anchor="right" open={open} classes={{paper: classes.drawerPaper,}}>
+                    <DetailPopup/>
+                </Drawer>
+            </ClickAwayListener>
+        </div>
         
+        // <SFullScreen>
+        //     <Maps />
+        //     <DetailPopup/>
+        // </SFullScreen>
     )
 }
 export default MapsContainer;
--- src/modules/Maps.js
+++ src/modules/Maps.js
@@ -2,15 +2,14 @@
 import 'leaflet/dist/leaflet.css';
 import '../dist/Maps.css';
 import React, { useEffect, useState } from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, Tooltip, ZoomControl, useMapEvents } from 'react-leaflet';
 import L from 'leaflet';
 import icon from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 
 
-function Maps(props) {
-    console.log("height")
-    console.log(props)
+
+function Maps() {
     const [lat, setLat] = useState('37.576429');
     const [lng, setLng] = useState('126.904376');
     const [zoom, setZoom] = useState(13);
@@ -19,7 +18,6 @@ function Maps(props) {
 
     const position2 = [37.629889, 127.067015]; //
 
-
     let DefaultIcon = L.icon({
         iconUrl: icon,
         shadowUrl: iconShadow
@@ -27,20 +25,17 @@ function Maps(props) {
 
     L.Marker.prototype.options.icon = DefaultIcon;
     return (
-        <MapContainer style={{ width: '100%', height: 'calc(100vh - 60px)' }} center={center} zoom={zoom}>
+        <MapContainer style={{ width: '100%', height: 'calc(100vh - 60px)' }} center={center} zoom={zoom} zoomControl={false} minZoom={7}>
             <TileLayer
                 attribution='<a href="https://maps.google.com/maps"></a>'
                 url='http://mt0.google.com/vt/lyrs=m&hl=kr&x={x}&y={y}&z={z}'
             />
+            <ZoomControl position="bottomleft" />
             <Marker position={position}>
-                <Popup>
-                    <span>콩이네입니다.</span>
-                </Popup>
+                <Tooltip><span>콩이네입니다.</span></Tooltip>
             </Marker>
             <Marker position={position2}>
-                <Popup>
-                    <span>뚱이네입니다.</span>
-                </Popup>
+                <Tooltip><span>뚱이네입니다.</span></Tooltip>
             </Marker>
         </MapContainer>
 
