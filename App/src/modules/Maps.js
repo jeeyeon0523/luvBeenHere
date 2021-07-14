ea63ca9da02627a20822df1f7149b02f10c24a14
Jin <jeeyeon0523@gmail.com>
2021-07-14 15:17:35 +0900

    add map boundaries

--- App/src/modules/Maps.js
+++ App/src/modules/Maps.js
@@ -1,10 +1,21 @@
 import 'leaflet/dist/leaflet.css';
 import '../Styles/dist/Maps.css';
 import React, { useEffect, useState } from 'react';
-import { MapContainer, TileLayer, Marker, Popup, Tooltip, ZoomControl, useMapEvents } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, Tooltip, ZoomControl, useMapEvents, useMap } from 'react-leaflet';
 import L from 'leaflet';
 import icon from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+
+function MapHooks(){
+    const map = useMap();
+    // set map bounds
+    const LeftTop = L.latLng(38.807100, 123.753453);
+    const RightBottom = L.latLng(32.019165, 132.125035);
+    const bounds = L.latLngBounds(LeftTop, RightBottom);
+    map.setMaxBounds(bounds);
+    return null;
+}
 
 function Maps(props) {
     const [lat, setLat] = useState('37.576429');
@@ -15,11 +26,11 @@ function Maps(props) {
 
     const position2 = [37.629889, 127.067015]; //
 
+    // set default marker Icons
     let DefaultIcon = L.icon({
         iconUrl: icon,
         shadowUrl: iconShadow
     });
-
     L.Marker.prototype.options.icon = DefaultIcon;
 
     const handleMarkerClick = () => {
@@ -28,6 +39,7 @@ function Maps(props) {
 
     return (
         <MapContainer style={{ width: '100%', height: 'calc(100vh - 60px)' }} center={center} zoom={zoom} zoomControl={false} minZoom={7}>
+            <MapHooks/>
             <TileLayer
                 attribution='<a href="https://maps.google.com/maps"></a>'
                 url='http://mt0.google.com/vt/lyrs=m&hl=kr&x={x}&y={y}&z={z}'
